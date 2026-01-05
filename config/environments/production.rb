af792e616ee73f2fb9cda2f877f2edb83a940e41
Johnathan Lyman <jelyman@gmail.com>
2026-01-04 22:00:33 -0800

    add hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,11 @@ Rails.application.configure do
   ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "lhwx-e1aba94eba7d.herokuapp.com",
+    "leahillwx.org",     # Allow requests from leahillwx.org
+    /.*\.leahillwx\.org/ # Allow requests from subdomains like `www.leahillwx.org`
+  ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
