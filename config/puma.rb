31e14fe1d82db767d3255ff47435fb0689025efb
Johnathan Lyman <jelyman@gmail.com>
2026-01-04 22:25:56 -0800

    no workers

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,6 @@ require "barnes"
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
-workers ENV.fetch("WEB_CONCURRENCY", 1)
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
