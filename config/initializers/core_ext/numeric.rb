ffae39b1414df3fb563b166dae03dc3af9c09b2a
Johnathan Lyman <jelyman@gmail.com>
2026-01-06 21:19:53 -0800

    add to_mph

--- config/initializers/core_ext/numeric.rb
+++ config/initializers/core_ext/numeric.rb
@@ -7,6 +7,10 @@ class Numeric
     ((self - 32) * 5.0 / 9.0).round(2)
   end
 
+  def to_mph
+    self * 1.60934
+  end
+
   alias_method :to_fh, :to_fahrenheit
   alias_method :to_c, :to_celsius
 end
