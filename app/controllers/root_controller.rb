2b6c4c3f82e7b3b6cb36aab87a4fe97b46adf895
Johnathan Lyman <jelyman@gmail.com>
2026-01-11 09:40:33 -0800

    update about page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -6,5 +6,6 @@ class RootController < ApplicationController
 
   def about
     @weather_measurement_count = WeatherMeasurement.count
+    @first_weather_measurement = WeatherMeasurement.order(reading_date_time: :asc).first
   end
 end
