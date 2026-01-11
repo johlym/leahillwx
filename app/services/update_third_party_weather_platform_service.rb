0ae6815cbdffd110ef7245da22e94a3c02948f5e
Johnathan Lyman <jelyman@gmail.com>
2026-01-11 12:12:50 -0800

    start work on WU syncing

--- app/services/update_third_party_weather_platform_service.rb
+++ app/services/update_third_party_weather_platform_service.rb
@@ -23,17 +23,54 @@ class UpdateThirdPartyWeatherPlatformService
   end
 
   def update_weatherunderground(measurement)
+    # TODO: Implement update_weatherunderground
+    # SAMPLE POST URL with data embedded: https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=KCASANFR5&PASSWORD=XXXXXX&dateutc=2000-01-01+10%3A32%3A35&winddir=230&windspeedmph=12&windgustmph=12&tempf=70&rainin=0&baromin=29.1&dewptf=68.2&humidity=90&weather=&clouds=&softwaretype=vws%20versionxx&action=updateraw
+
+    date_utc = measurement.reading_date_time.utc.strftime("%Y-%m-%d+%H:%M:%S")
+    wind_dir = measurement.wind_dir
+    wind_speed_mph = measurement.wind_speed_mph
+    wind_gust_mph = measurement.wind_gust_mph
+    temp_f = measurement.temperature_f
+    rain_in = measurement.rain_rate_in
+    barometer_in = measurement.barometer_abs_mmhg
+    humidity = measurement.humidity
+    dew_point_f = measurement.dew_point_f
+
+    request_url = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
+    request_params = {
+      ID: ENV["WU_STATION_ID"],
+      PASSWORD: ENV["WU_STATION_KEY"],
+      dateutc: date_utc,
+      winddir: wind_dir,
+      windspeedmph: wind_speed_mph,
+      windgustmph: wind_gust_mph,
+      tempf: temp_f,
+      rainin: rain_in,
+      baromin: barometer_in,
+      dewptf: dew_point_f,
+      humidity: humidity,
+      weather: "",
+      clouds: "",
+      softwaretype: "lhwx",
+      action: "updateraw"
+  }
+    wu_request = HTTParty.post(request_url, query: request_params)
+    Rails.logger.info "Weather Underground response: #{wu_request.body}"
   end
 
   def update_pwsweather(measurement)
+    # TODO: Implement update_pwsweather
   end
 
   def update_awekas(measurement)
+    # TODO: Implement update_awekas
   end
 
-  def weathercloud(measurement)
+  def update_weathercloud(measurement)
+    # TODO: Implement update_weathercloud
   end
 
   def cwop(measurement)
+    # TODO: Implement cwop
   end
 end
--- app/sidekiq/update_weather_underground_job.rb
+++ app/sidekiq/update_weather_underground_job.rb
@@ -3,6 +3,7 @@ class UpdateWeatherUndergroundJob
   sidekiq_options retry: false
 
   def perform(*args)
+    nil unless ENV["WU_STATION_ID"] && ENV["WU_STATION_KEY"]
     # Do something
   end
 end
