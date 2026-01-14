ab0584245fbc55969b07d04b62c2a83f75b2d211
Johnathan Lyman <jelyman@gmail.com>
2026-01-13 16:02:43 -0800

    ensure that if no forecasts eixst, we fetch one on command

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,17 @@
 class RootController < ApplicationController
   def index
-    @current = WeatherMeasurement.order(reading_date_time: :desc).first
-    @forecast = ForecastParserService.new(Forecast.last).parse
+    @current = WeatherMeasurement
+      .select("weather_measurements.*, (SELECT COUNT(*) FROM weather_measurements) as total_count")
+      .order(reading_date_time: :desc)
+      .first
+
+    forecast_record = Forecast.last
+    if forecast_record.nil?
+      ow_forecast = OpenWeatherApiService.new.retrieve_forecast
+      forecast_record = Forecast.create(forecast: ow_forecast)
+    end
+
+    @forecast = ForecastParserService.new(forecast_record).parse
   end
 
   def about
