2b2f7de843fa0ebbb12787d8c7db07aca1e86d6a
Johnathan Lyman <jelyman@gmail.com>
2026-01-13 16:28:02 -0800

    add counter to show progress

--- app/sidekiq/remove_duplicate_weather_measurements_job.rb
+++ app/sidekiq/remove_duplicate_weather_measurements_job.rb
@@ -9,7 +9,12 @@ class RemoveDuplicateWeatherMeasurementsJob
 
     Rails.logger.info("Duplicate entries found: #{duplicate_date_times.count}")
 
+    count = duplicate_date_times.count
+    counter = 0
+
     duplicate_date_times.each do |date_time|
+      counter += 1
+      Rails.logger.info("#{counter}/#{count} Removing duplicates for #{date_time}")
       records = WeatherMeasurement.where(reading_date_time: date_time).order(:id)
       records_to_delete = records.offset(1)
       records_to_delete.destroy_all
