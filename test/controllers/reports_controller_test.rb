43f8540977aa684fce2201617de02d2e06cd9bde
Johnathan Lyman <jelyman@gmail.com>
2026-01-13 18:05:08 -0800

    fix tests

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -2,7 +2,22 @@ require "test_helper"
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @report = Report.create!(year: 2024, month: 1)
+    @report = Report.create!(
+      year: 2024,
+      month: 1,
+      month_mean_temp: 10.0,
+      month_high_temp: 15.0,
+      month_high_temp_day: 15,
+      month_low_temp: 5.0,
+      month_low_temp_day: 1,
+      total_heat_degree_days: 100.0,
+      total_cool_degree_days: 0.0,
+      total_rain: 2.5,
+      avg_wind_speed: 5.0,
+      month_high_wind_speed: 15.0,
+      month_high_wind_day: 10,
+      dominant_wind_dir: 180
+    )
     @report.entries.create!(day: 15, mean_temp: 50.0, high_temp: 60.0, low_temp: 40.0)
   end
 
@@ -29,7 +44,7 @@ class ReportsControllerTest < ActionDispatch::IntegrationTest
   test "should show report" do
     get report_url(2024, "january")
     assert_response :success
-    assert_select "h2", text: "January 2024"
+    assert_select "h1 small", text: "for January 2024"
   end
 
   test "should return 404 for invalid month name" do
