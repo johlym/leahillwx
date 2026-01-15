86a87e24210e126205b06be6c36f09e951b972f6
Johnathan Lyman <jelyman@gmail.com>
2026-01-14 18:49:38 -0800

    fix test issue

--- app/components/reports/report_navigation_component.rb
+++ app/components/reports/report_navigation_component.rb
@@ -6,4 +6,14 @@ class Reports::ReportNavigationComponent < ViewComponent::Base
     @current_month = current_month
     @current_day = current_day
   end
+
+  def show_download_link?
+    @current_year.present? && @current_month.present?
+  end
+
+  def month_name
+    return nil unless @current_month
+
+    Date::MONTHNAMES[@current_month].downcase
+  end
 end
