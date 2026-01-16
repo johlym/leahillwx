32ae7fafaf847b11690d200f8d2583d97161b10e
Johnathan Lyman <jelyman@gmail.com>
2026-01-15 22:11:28 -0800

    update component tests for new visuals

--- test/components/ui/earthquake_table_component_test.rb
+++ test/components/ui/earthquake_table_component_test.rb
@@ -7,27 +7,27 @@ class Home::EarthquakeTableComponentTest < ViewComponent::TestCase
     earthquakes = [ earthquakes(:one) ]
     render_inline(Home::EarthquakeTableComponent.new(earthquakes: earthquakes))
 
-    assert_selector "table.earthquake-table"
-    assert_selector "thead"
-    assert_selector "tbody"
+    assert_selector "div.earthquake-table"
+    assert_selector "div.eq-header"
+    assert_selector "div.eq-body"
   end
 
   test "renders table headers" do
     render_inline(Home::EarthquakeTableComponent.new(earthquakes: []))
 
-    assert_selector "th", text: "Mag"
-    assert_selector "th", text: "Time"
-    assert_selector "th", text: "Location"
-    assert_selector "th", text: "Distance"
-    assert_selector "th", text: "Depth"
+    assert_selector "div.eq-header-cell", text: "Mag"
+    assert_selector "div.eq-header-cell", text: "Timestamp"
+    assert_selector "div.eq-header-cell", text: "Location"
+    assert_selector "div.eq-header-cell", text: "Distance"
+    assert_selector "div.eq-header-cell", text: "Depth"
   end
 
   test "renders earthquake data" do
     earthquake = earthquakes(:one)
     render_inline(Home::EarthquakeTableComponent.new(earthquakes: [ earthquake ]))
 
-    assert_selector "td.eq-mag-cell", text: earthquake.magnitude.to_s
-    assert_selector "td", text: earthquake.place
+    assert_selector "div.eq-mag-cell", text: earthquake.magnitude.to_s
+    assert_selector "div.eq-cell", text: earthquake.place
   end
 
   test "renders formatted time" do
@@ -37,29 +37,29 @@ class Home::EarthquakeTableComponentTest < ViewComponent::TestCase
     expected_date = earthquake.eventtime.in_time_zone("America/Los_Angeles").strftime("%B %d, %Y")
     expected_time = earthquake.eventtime.in_time_zone("America/Los_Angeles").strftime("%I:%M %p")
 
-    assert_selector "td", text: /#{Regexp.escape(expected_date)}/
-    assert_selector "td", text: /#{Regexp.escape(expected_time)}/
+    assert_selector "div.eq-cell", text: /#{Regexp.escape(expected_date)}/
+    assert_selector "div.eq-cell", text: /#{Regexp.escape(expected_time)}/
   end
 
   test "renders distance with precision" do
     earthquake = earthquakes(:one)
     render_inline(Home::EarthquakeTableComponent.new(earthquakes: [ earthquake ]))
 
-    assert_selector "td", text: /999\.99 mi/
+    assert_selector "div.eq-cell", text: /999\.99 mi/
   end
 
   test "renders depth with precision" do
     earthquake = earthquakes(:one)
     render_inline(Home::EarthquakeTableComponent.new(earthquakes: [ earthquake ]))
 
-    assert_selector "td", text: /100\.00 mi/
+    assert_selector "div.eq-cell", text: /100\.00 mi/
   end
 
   test "renders USGS link" do
     earthquake = earthquakes(:one)
     render_inline(Home::EarthquakeTableComponent.new(earthquakes: [ earthquake ]))
 
-    assert_selector "a[href='#{earthquake.url}'][target='blank']", text: /View at.*USGS/m
+    assert_selector "a[href='#{earthquake.url}'][target='blank']", text: earthquake.place
   end
 
   test "renders multiple earthquakes" do
@@ -72,18 +72,20 @@ class Home::EarthquakeTableComponentTest < ViewComponent::TestCase
       depth: 10.0,
       lat: 35.0,
       lon: -119.0,
-      url: "https://example.com/2"
+      url: "https://example.com/2",
+      last_updated: Time.current,
+      usgs_id: "test123"
     )
 
     render_inline(Home::EarthquakeTableComponent.new(earthquakes: [ earthquake1, earthquake2 ]))
 
-    assert_selector "tbody tr", count: 2
+    assert_selector "div.eq-row", count: 2
   end
 
   test "renders empty table when no earthquakes" do
     render_inline(Home::EarthquakeTableComponent.new(earthquakes: []))
 
-    assert_selector "table.earthquake-table"
-    assert_selector "tbody tr", count: 0
+    assert_selector "div.earthquake-table"
+    assert_selector "div.eq-row", count: 0
   end
 end
