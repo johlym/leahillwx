67e543c8ab3647887f23d98e2ef7e31eb6ff9937
Johnathan Lyman <jelyman@gmail.com>
2026-01-19 14:40:52 -0800

    refine apparent temperature calculation with solar radiation effects and threshold adjustments

--- app/models/concerns/feels_like.rb
+++ app/models/concerns/feels_like.rb
@@ -1,13 +1,13 @@
 module FeelsLike
   extend ActiveSupport::Concern
 
-  def feels_like_c(temp_c:, humidity:, wind_speed_mps:)
+  def feels_like_c(temp_c:, humidity:, wind_speed_mps:, cloud_pct:, is_daytime:)
     if cold_enough_for_wind_chill?(temp_c, wind_speed_mps)
       wind_chill_c(temp_c, wind_speed_mps)
     elsif hot_enough_for_heat_index?(temp_c, humidity)
       heat_index_c(temp_c, humidity)
     else
-      temp_c
+      apparent_temperature_c(temp_c, humidity, wind_speed_mps, cloud_pct, is_daytime)
     end
   end
 
@@ -15,7 +15,7 @@ module FeelsLike
   # Wind Chill
   # --------------------
   def cold_enough_for_wind_chill?(temp_c, wind_speed_mps)
-    temp_c <= 10 && wind_speed_mps > 1.3
+    temp_c <= 10.0 && wind_speed_mps >= 1.3
   end
 
   def wind_chill_c(temp_c, wind_speed_mps)
@@ -34,7 +34,7 @@ module FeelsLike
   # Heat Index
   # --------------------
   def hot_enough_for_heat_index?(temp_c, humidity)
-    temp_c >= 26.67 && humidity >= 40
+    temp_c >= 27.0 && humidity >= 40
   end
 
   def heat_index_c(temp_c, humidity)
@@ -52,10 +52,54 @@ module FeelsLike
       0.00085282 * temp_f * rh**2 -
       0.00000199 * temp_f**2 * rh**2
 
-      hi_c = f_to_c(hi_f)
-      [ hi_c, temp_c ].max
+    hi_c = f_to_c(hi_f)
+    [ hi_c, temp_c ].max
   end
 
+  # --------------------
+  # Apparent Temperature (Steadman)
+  # --------------------
+  def apparent_temperature_c(temp_c, humidity, wind_speed_mps, cloud_pct, is_daytime)
+    e_hpa = vapor_pressure(temp_c, humidity)
+
+    apparent_temp =
+      temp_c +
+      0.33 * e_hpa -
+      0.70 * wind_speed_mps -
+      4.00
+
+    if should_apply_solar_adjustment?(temp_c, wind_speed_mps, cloud_pct, is_daytime)
+      apparent_temp += solar_adjustment(cloud_pct)
+    end
+
+    apparent_temp
+  end
+
+  def vapor_pressure(temp_c, humidity_pct)
+    (humidity_pct / 100.0) *
+      6.105 *
+      Math.exp((17.27 * temp_c) / (237.7 + temp_c))
+  end
+
+  def should_apply_solar_adjustment?(temp_c, wind_speed_mps, cloud_pct, is_daytime)
+    is_daytime &&
+      wind_speed_mps < 2.0 &&
+      cloud_pct < 40 &&
+      temp_c >= 0.0 &&
+      temp_c <= 15.0
+  end
+
+  def solar_adjustment(cloud_pct)
+    clear_sky_factor = (40.0 - cloud_pct) / 40.0
+    clear_sky_factor = [ [ clear_sky_factor, 0.0 ].max, 1.0 ].min
+
+    solar_boost = 0.5 + (clear_sky_factor * 3.5)
+    [ [ solar_boost, 0.0 ].max, 4.0 ].min
+  end
+
+  # --------------------
+  # Utilities
+  # --------------------
   def c_to_f(c)
     c * 9.0 / 5.0 + 32
   end
