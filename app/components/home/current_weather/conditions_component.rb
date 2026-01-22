87331778c6912b390c6918aede423082e8eb7890
Johnathan Lyman <jelyman@gmail.com>
2026-01-21 21:08:13 -0800

    continue work on current section

--- app/components/home/current_weather/conditions_component.rb
+++ app/components/home/current_weather/conditions_component.rb
@@ -23,8 +23,70 @@ class Home::CurrentWeather::ConditionsComponent < ViewComponent::Base
     @current.wind_speed.round(0)
   end
 
+  def current_wind_direction
+    case @current.wind_dir
+    when 0..22.5
+      "N"
+    when 22.5..67.5
+      "NE"
+    when 67.5..112.5
+      "E"
+    when 112.5..157.5
+      "SE"
+    when 157.5..202.5
+      "S"
+    when 202.5..247.5
+      "SW"
+    when 247.5..292.5
+      "W"
+    when 292.5..337.5
+      "NW"
+    when 337.5..360
+      "N"
+    end
+  end
+
   def current_humidity
     @current.humidity
+  end
+
+  def current_uvi
+    @current.uvi.round
+  end
+
+  def current_uv_index_category
+    case @current.uv
+    when 0..2
+      "Low"
+    when 3..5
+      "Moderate"
+    when 6..7
+      "High"
+    when 8..10
+      "Very High"
+    when 11..
+      "Extreme"
+    end
+  end
+
+  def current_sunrise
+    @almanac.sunrise_at.in_time_zone("America/Los_Angeles").strftime("%I:%M %p")
+  end
+
+  def current_sunset
+    @almanac.sunset_at.in_time_zone("America/Los_Angeles").strftime("%I:%M %p")
+  end
+
+  def current_rain_day
+    @current.rain_day
+  end
+
+  def current_rain_hour
+    @current.rain_rate
+  end
+
+  def current_dew_point
+    @current.dew_point.to_fahrenheit.round(0)
   end
 
   def hourly_forecast
