05c98e2819d78e8aecdfd2250f738d6cfce2728b
Johnathan Lyman <jelyman@gmail.com>
2026-07-13 21:07:29 -0700

    lint

--- app/models/concerns/epa_pm25_aqi.rb
+++ app/models/concerns/epa_pm25_aqi.rb
@@ -7,22 +7,22 @@ module EpaPm25Aqi
 
   # [BPLo, BPHi, ILo, IHi]
   PM25_BREAKPOINTS = [
-    [0.0, 12.0, 0, 50],
-    [12.1, 35.4, 51, 100],
-    [35.5, 55.4, 101, 150],
-    [55.5, 150.4, 151, 200],
-    [150.5, 250.4, 201, 300],
-    [250.5, 350.4, 301, 400],
-    [350.5, 500.4, 401, 500]
+    [ 0.0, 12.0, 0, 50 ],
+    [ 12.1, 35.4, 51, 100 ],
+    [ 35.5, 55.4, 101, 150 ],
+    [ 55.5, 150.4, 151, 200 ],
+    [ 150.5, 250.4, 201, 300 ],
+    [ 250.5, 350.4, 301, 400 ],
+    [ 350.5, 500.4, 401, 500 ]
   ].freeze
 
   CATEGORIES = [
-    [0, 50, "Good"],
-    [51, 100, "Moderate"],
-    [101, 150, "Unhealthy for Sensitive Groups"],
-    [151, 200, "Unhealthy"],
-    [201, 300, "Very Unhealthy"],
-    [301, nil, "Hazardous"]
+    [ 0, 50, "Good" ],
+    [ 51, 100, "Moderate" ],
+    [ 101, 150, "Unhealthy for Sensitive Groups" ],
+    [ 151, 200, "Unhealthy" ],
+    [ 201, 300, "Very Unhealthy" ],
+    [ 301, nil, "Hazardous" ]
   ].freeze
 
   module ClassMethods
